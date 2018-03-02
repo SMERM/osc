5b65082d213300ebc63872a580ae5d78373314cd
Davide Tedesco <davide@MacBook-Pro.local>
2018-03-02 15:06:26 +0100

    risolto un problema se ne fa n'antro

--- src/audio_output.c
+++ src/audio_output.c
@@ -12,7 +12,7 @@ pa_check(int result, const char *caller)
 {
   if( result <= 0 && result != paNoError )
   {
-    printf(  "%s PortAudio error: %s\n", caller, Pa_GetErrorText( result ) );
+    printf(  "%s PortAudio error n.%d: %s\n", caller,result ,Pa_GetErrorText( result ) );
     abort();
   }
   return result;
@@ -29,7 +29,7 @@ pa_device_chooser ()
   {
     const   PaDeviceInfo *deviceInfo;
     deviceInfo = Pa_GetDeviceInfo( i );
-    printf("device %d: %s\n", i, deviceInfo->name );
+    printf("device %d: %s (%d)\n", deviceInfo->hostApi, deviceInfo->name, deviceInfo->maxOutputChannels );
   }
 }
 
@@ -46,7 +46,7 @@ pa_audio_output (const void *input,
     OSCVAR *out = (OSCVAR*)output;
     unsigned int i;
     (void) input; /* Prevent unused variable warning. */
-
+puts(".");
     for( i=0; i<frameCount; i++ )
     {
         Sample *s = osc_compute(&data->s,&data->o);
@@ -57,15 +57,17 @@ pa_audio_output (const void *input,
 }
 
 static int
-open_audio_stream()
+open_audio_stream(PaStream *stream)
 {
-    PaStream *stream;
     PaStreamParameters outputParameters = {0};
     PaStreamParameters inputParameters = {0};
     /* FIXME brutal hack */
+    inputParameters.channelCount = 1;
     outputParameters.channelCount = 1;
-    outputParameters.device = 1;
-    outputParameters.sampleFormat = paFloat32;
+/*  outputParameters.device = 1; */
+    pa_check(outputParameters.device = Pa_GetDefaultOutputDevice(),"Pa_GetDefaultOutputDevice");
+    inputParameters.sampleFormat = paInt16;
+    outputParameters.sampleFormat = paInt16;
     return Pa_OpenStream( &stream,
                           &inputParameters,
                           &outputParameters,
@@ -82,12 +84,13 @@ open_audio_stream()
                           &__data__ ); /*This is a pointer that will be passed to
                                                    your callback*/
 }
-
+static PaStream *__stream__=0;
 void
 audio_output()
 {
-    pa_check(open_audio_stream(),"audio_output");
-    sleep (10000); /* FIXME : brutal hack */
+    pa_check(open_audio_stream(__stream__),"open_audio_stream");
+    pa_check(Pa_StartStream(__stream__),"Pa_StartStream");
+    Pa_Sleep (1000);
 }
 
 void
@@ -101,5 +104,6 @@ audio_output_initialize()
 int
 audio_output_terminate()
 {
+  Pa_StopStream(__stream__);
   return pa_check(Pa_Terminate(),"audio_output_terminate");
 }
